5f45a744f6b3c62b2f31a008acf9d357d7340bd6
Denis Lebedev <dl@isfb.ru>
2015-12-24 13:31:08 +0300

    Added payment end pages and theming

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
 
   get '/yandexkassa/:gateway_id/:order_id' => 'yandexkassa#show', as: :yandexkassa
+  get  '/yandexkassa/success_payment' => 'yandexkassa#success_payment', as: :success_payment
+  get  '/yandexkassa/failed_payment' => 'yandexkassa#failed_payment', as: :failed_payment
   post '/yandexkassa/check_order' => 'yandexkassa#check_order'
   post '/yandexkassa/payment_aviso' => 'yandexkassa#payment_aviso'
   post '/yandexkassa/test_check_order' => 'yandexkassa#check_order'
