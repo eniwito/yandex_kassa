67b6e601ac8365597db8dbdceb2f7e745f4265bd
alexanderpower <aleksandr.power88@gmail.com>
2015-12-24 17:27:47 +0500

    Added temp routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,17 @@ Spree::Core::Engine.routes.draw do
   get '/yandexkassa/:gateway_id/:order_id' => 'yandexkassa#show', as: :yandexkassa
   get  '/yandexkassa/success_payment' => 'yandexkassa#success_payment', as: :success_payment
   get  '/yandexkassa/failed_payment' => 'yandexkassa#failed_payment', as: :failed_payment
+
+  # Временно
+  get  '/yandexkassa/success_order' => 'yandexkassa#success_payment'
+  get  '/yandexkassa/failed_order' => 'yandexkassa#failed_payment'
+
   post '/yandexkassa/check_order' => 'yandexkassa#check_order'
   post '/yandexkassa/payment_aviso' => 'yandexkassa#payment_aviso'
+
   # Временно
   post '/yandexkassa/aviso_order' => 'yandexkassa#payment_aviso'
+
   post '/yandexkassa/test_check_order' => 'yandexkassa#check_order'
   post '/yandexkassa/test_payment_aviso' => 'yandexkassa#payment_aviso'
 end
