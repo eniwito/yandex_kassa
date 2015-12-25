f62b9a9b960d8f10c3b826af1396c719caf9e2db
alexanderpower <aleksandr.power88@gmail.com>
2015-12-25 19:00:54 +0500

    Added helper method checked_payment_methods.

--- app/helpers/spree/yandexkassa_helper.rb
+++ app/helpers/spree/yandexkassa_helper.rb
@@ -2,11 +2,17 @@ module Spree::YandexkassaHelper
   # Info field that will be located near user id on the yandex kassa's page
   def yandex_kassa_identity_method(user)
     yandex_kassa_identity_field = Rails.application.config.yandex_kassa_identity
-     if user.respond_to? yandex_kassa_identity_field
-       user.public_send yandex_kassa_identity_field
-     else
-       logger.warning "Yandex kassa identity field #{yandex_kassa_identity_field} not found for user. Using email field instead"
-       user.email
-     end
+    if user.respond_to? yandex_kassa_identity_field
+      user.public_send yandex_kassa_identity_field
+    else
+      logger.warning "Yandex kassa identity field #{yandex_kassa_identity_field} not found for user. Using email field instead"
+      user.email
+    end
+  end
+
+  # @return [Array] of symbols checked payment methods.
+  # For ex. [:payment_method_AC, :payment_method_MC, :payment_method_PB]
+  def checked_payment_methods(gateway)
+    gateway.defined_preferences.select { |p| p.match /payment_method_/ }.select { |p| gateway.options[p] }
   end
 end
