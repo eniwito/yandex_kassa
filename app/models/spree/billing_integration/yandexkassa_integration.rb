95cf8ef1d8111a4fd7d03e9b067e3fe65ba089bf
alexanderpower <aleksandr.power88@gmail.com>
2015-12-25 18:05:46 +0500

    Added all payment methods.

--- app/models/spree/billing_integration/yandexkassa_integration.rb
+++ app/models/spree/billing_integration/yandexkassa_integration.rb
@@ -4,6 +4,9 @@ module Spree
     preference :scid, :string
     preference :password, :string
 
+    # Creating all payment methods
+    %w(AC PC MC GP WM SB MP AB MA PB QW KV QP).each { |method| preference "payment_method_#{method}".to_sym, :boolean }
+
     def provider_class
       self.class
     end
